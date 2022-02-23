67aa4a6102cddb3ed62d1bfc55f9dd7057847d35
nazar.slobodian <nazar.slobodian@intive.com>
2022-02-23 19:58:00 +0100

    Zombie tests

--- src/zombie/zombie.service.spec.ts
+++ src/zombie/zombie.service.spec.ts
@@ -1,12 +1,27 @@
+import { getModelToken } from '@nestjs/mongoose';
 import { Test, TestingModule } from '@nestjs/testing';
+import { Zombie } from './schema/zombie.schema';
+import { ZombieRepo } from './zombie.repo';
 import { ZombieService } from './zombie.service';
+
+const ZombieModel = {
+  findAll: jest.fn().mockResolvedValue([]),
+  create: jest.fn().mockResolvedValue({}),
+};
 
 describe('ZombieService', () => {
   let service: ZombieService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ZombieService],
+      providers: [
+        ZombieService,
+        ZombieRepo,
+        {
+          provide: getModelToken(Zombie.name),
+          useValue: ZombieModel,
+        },
+      ],
     }).compile();
 
     service = module.get<ZombieService>(ZombieService);
@@ -14,5 +29,11 @@ describe('ZombieService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('create new zombie', async () => {
+    const response = await service.create({ name: 'test' });
+
+    expect(response).toEqual({});
   });
 });
